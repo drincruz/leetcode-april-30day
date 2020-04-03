83dcfd27b1e72c3e1b18506a610d584f489b651a
Adrian Cruz <drincruz@gmail.com>
2020-04-03 17:36:38 -0400

    Cargo New Project
    Just adding initial `leetcode` rust project.

---
+++ rust/leetcode/target/debug/deps/leetcode-5e83d3c79f80d419.d
@@ -0,0 +1,5 @@
+/Users/adriancruz/src/github.com/drincruz/leetcode-april-30day/rust/leetcode/target/debug/deps/leetcode-5e83d3c79f80d419: src/main.rs
+
+/Users/adriancruz/src/github.com/drincruz/leetcode-april-30day/rust/leetcode/target/debug/deps/leetcode-5e83d3c79f80d419.d: src/main.rs
+
+src/main.rs:
---
+++ rust/leetcode/target/debug/leetcode.d
@@ -0,0 +1,1 @@
+/Users/adriancruz/src/github.com/drincruz/leetcode-april-30day/rust/leetcode/target/debug/leetcode: /Users/adriancruz/src/github.com/drincruz/leetcode-april-30day/rust/leetcode/src/main.rs
