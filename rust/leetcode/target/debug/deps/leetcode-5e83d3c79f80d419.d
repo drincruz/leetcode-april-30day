27cfb593a7f4059a69cf2725a3febc0d675db9ff
Adrian Cruz <drincruz@gmail.com>
2020-04-04 20:25:03 -0400

    [rust] Remove `target` Directory
    I put the .gitignore file from #2 and I accidentally committed the target directory beforehand.

--- rust/leetcode/target/debug/deps/leetcode-5e83d3c79f80d419.d
+++
@@ -1,5 +0,0 @@
-/Users/adriancruz/src/github.com/drincruz/leetcode-april-30day/rust/leetcode/target/debug/deps/leetcode-5e83d3c79f80d419: src/main.rs
-
-/Users/adriancruz/src/github.com/drincruz/leetcode-april-30day/rust/leetcode/target/debug/deps/leetcode-5e83d3c79f80d419.d: src/main.rs
-
-src/main.rs:
--- rust/leetcode/target/debug/leetcode.d
+++
@@ -1,1 +0,0 @@
-/Users/adriancruz/src/github.com/drincruz/leetcode-april-30day/rust/leetcode/target/debug/leetcode: /Users/adriancruz/src/github.com/drincruz/leetcode-april-30day/rust/leetcode/src/main.rs
